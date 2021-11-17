a7d5ee3d267f4b9acf1302f70f01f8bac76838b2
Nikoletta Rácz <racznikoletta98@gmail.com>
2021-11-17 22:51:28 +0100

    update wordpracticefragment and sequencepractice

--- app/src/main/java/com/example/fogas/wordPracticeFragment.java
+++ app/src/main/java/com/example/fogas/wordPracticeFragment.java
@@ -1,21 +1,38 @@
 package com.example.fogas;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+import static io.realm.Realm.getApplicationContext;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fogas.Models.PegDataModel;
 import com.example.fogas.Models.PegModel;
 import com.example.fogas.Models.UserDataModel;
 
+import java.util.zip.Inflater;
+
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,6 +51,13 @@ public class wordPracticeFragment extends Fragment {
     private String mParam2;
     private Realm wordRealm;
     boolean ifExists = false;
+    private TextView kerdes;
+    private int index = 0;
+    private String nyelv;
+    private String [] szavak = new String[10];
+    private TextView szoveg_bevitel;
+    private TextView teszt;
+    private  TextView hiba_szo;
 
     public wordPracticeFragment() {
         // Required empty public constructor
@@ -59,11 +83,15 @@ public class wordPracticeFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
         /*if(!checkIfWordsInDatabase())
 
             if(getContext()!= null) {
@@ -86,41 +114,167 @@ public class wordPracticeFragment extends Fragment {
         }*/
     }
 
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setRetainInstance(true);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_word_practice, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_word_practice,container,false);
+        kerdes = (TextView) view.findViewById(R.id.szoveg_kerdes);
+        szoveg_bevitel = (TextView) view.findViewById(R.id.szoveg_bevitel);
+        hiba_szo = (TextView) view.findViewById(R.id.hiba_szo);
+
+
+
+        view.findViewById(R.id.szoveg_gomb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teszt = (TextView) view.findViewById(R.id.teszt);
+                boolean ellenorzes = false;
+                if(index==0){
+                    hiba_szo.setVisibility(View.INVISIBLE);
+                    if(szoveg_bevitel.getText().toString().matches("[a-zA-Z]+")&& szoveg_bevitel.toString().length()>=1){
+                        szavak[index] = szoveg_bevitel.getText().toString();
+
+
+                        kerdes.setText(kerdes.getResources().getString(R.string.egyes_szo));
+                        ellenorzes = true;
+                    }
+                    else{
+                        hiba_szo.setVisibility(View.VISIBLE);
+
+                    }
+
+
+//hello
+
+                }
+                if(index!=0&&index<10){
+                    if(szoveg_bevitel.getText().toString().matches("[a-zA-Z]+")&& szoveg_bevitel.toString().length()>=1) {
+                        hiba_szo.setVisibility(View.INVISIBLE);
+                        if (index == 2) {
+                            kerdes.setText(kerdes.getResources().getString(R.string.kettes_szo));
+                        }
+                        if (index == 3) {
+                            kerdes.setText(kerdes.getResources().getString(R.string.harmas_szo));
+                        }
+                        if (index == 4) {
+                            kerdes.setText(kerdes.getResources().getString(R.string.negyes_szo));
+                        }
+                        if (index == 5) {
+                            kerdes.setText(kerdes.getResources().getString(R.string.otos_szo));
+                        }
+                        if (index == 6) {
+                            kerdes.setText(kerdes.getResources().getString(R.string.hatos_szo));
+                        }
+                        if (index == 7) {
+                            kerdes.setText(kerdes.getResources().getString(R.string.hetes_szo));
+                        }
+                        if (index == 8) {
+                            kerdes.setText(kerdes.getResources().getString(R.string.nyolcas_szo));
+                        }
+                        if (index == 9) {
+                            kerdes.setText(kerdes.getResources().getString(R.string.kilences_szo));
+                        }
+
+
+                        szavak[index] = szoveg_bevitel.getText().toString();
+
+                        String teszteles = "";
+                        for (int i = 0; i < 10; i++) {
+                            teszteles = teszteles + szavak[i] + "\n";
+                        }
+                        teszt.setText(teszteles);
+                        ellenorzes = true;
+                    }
+                    else{
+                        hiba_szo.setVisibility(View.VISIBLE);
+                    }
+
+                }
+                if(ellenorzes!=false) {
+                    index = index + 1;
+                }
+                if(index==10){
+                    hideKeyboard(getActivity()); //won't work
+
+                    try {
+                        FragmentManager fm = getFragmentManager();
+                        FragmentTransaction ft = fm.beginTransaction();
+                        ft.replace(R.id.container,new PracticeFragment(),"Add hints fragment")
+                                .addToBackStack(null)
+                                .commit();
+
+                    }catch(Throwable e){
+                        Log.d("practicefragment","letterpractice click error "+ e.toString());
+                    }
+
+
+                }
+            }
+        });
+
+
+
+
+
+
+
+        return view;
+
+
     }
 
     //TODO ne csak az elsőt nézze, hanem for ciklusban
     public boolean checkIfWordsInDatabase(){
         try{
-        wordRealm = Realm.getDefaultInstance();
-        wordRealm.executeTransaction(r->{
-            UserDataModel loggedUsr = wordRealm.where(UserDataModel.class).equalTo("loggedIn",true).findFirst();
-            if(loggedUsr != null) {
-                PegDataModel pegWord = wordRealm.where(PegDataModel.class).equalTo("userName", loggedUsr.getUserName()).findFirst();
-                if (pegWord != null) {
-                    PegModel firstPeg = pegWord.getOnePeg(0);
-                    if(firstPeg != null){
+            wordRealm = Realm.getDefaultInstance();
+            wordRealm.executeTransaction(r->{
+                UserDataModel loggedUsr = wordRealm.where(UserDataModel.class).equalTo("loggedIn",true).findFirst();
+                if(loggedUsr != null) {
+                    PegDataModel pegWord = wordRealm.where(PegDataModel.class).equalTo("userName", loggedUsr.getUserName()).findFirst();
+                    if (pegWord != null) {
+                        PegModel firstPeg = pegWord.getOnePeg(0);
+                        if(firstPeg != null){
                             if(firstPeg.getWord() != null){
                                 ifExists = true;
                             }
+                        }
                     }
-                }
 
 
                 }
 
-        });
+            });
 
-    }catch(Throwable e)
+        }catch(Throwable e)
         {
             Toast.makeText(getContext(), e.toString(), Toast.LENGTH_LONG).show();
             return false;
 
         }
-            return ifExists;
+        return ifExists;
     }
+
+
+
+
+
+
+
 }
